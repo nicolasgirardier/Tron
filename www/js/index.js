ce9314b63ddacfab1d91a8ed14266b9d36b19a67
Cloé <g.cloe.g@gmail.com>
2022-11-18 10:59:15 +0100

    test cloe

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
+    //test
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
