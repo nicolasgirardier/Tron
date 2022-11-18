bead0081e58fc56e9ccdd213fc7b645b507dbfa7
NICOLAS Girardier <Ngirardier@outlook.fr>
2022-11-18 10:29:44 +0100

    First commit

---
+++ www/js/index.js
@@ -0,0 +1,25 @@
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
+
+const ws = new WebSocket('ws://localhost:9898/');
+
+
+
+ws.onopen = function(e){
+    alert("Connexion établie !")
+}
+
+ws.onmessage = function(event) {
+    alert(event.data);
+  }
+
+function sendMessage(){
+    ws.send("Salut j'envoie un mess");
+}
