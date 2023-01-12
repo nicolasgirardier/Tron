f5a3b539ff6e119a2b2be3c57cdf6764c9cb8ab9
juliettedescotils <juliette.descotils@gmail.com>
2023-01-13 00:19:08 +0100

    viewEnding

--- www/client/views/viewEnding.js
+++ www/client/views/viewEnding.js
@@ -1,24 +1,32 @@
 ///////////////////ENDING////VIEW///////////////////
 function drawEndingRoom(names, cols, scores) {
     showView("ENDING");
+
     var table = document.getElementById("scoreBoard");
     var tblBody = document.createElement("tbody");
 
-    for(let i = 0; i<length(names);i++){
+    for(let i = 0; i<names.length;i++){
         let row = document.createElement("tr");
         let cell1 = document.createElement("td");
         let cell2 = document.createElement("td");
 
         let player = document.createTextNode(""+names[i]);
         let score = document.createTextNode(""+scores[i]);
-        player.style.color=""+cols[i]
+        cell1.style.color=cols[i]
 
         cell1.appendChild(player);
         cell2.appendChild(score);
-        row.appendChild(cell1,cell2)
+        row.appendChild(cell1);
+        row.appendChild(cell2)
         tblBody.appendChild(row);
         table.appendChild(tblBody);
-
     }
 }
+
+function emptyScoreBoard() {
+    while (table.lastElementChild)
+        table.removeChild(table.lastElementChild);
+}
+
+
 ////////////////////////////////////////////////////
