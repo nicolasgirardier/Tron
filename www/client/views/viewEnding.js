3c57b75c20b0f008dd71d0f9c1bb946535c5685d
juliettedescotils <juliette.descotils@gmail.com>
2023-01-13 01:45:54 +0100

    bug commit

--- www/client/views/viewEnding.js
+++ www/client/views/viewEnding.js
@@ -1,25 +1,26 @@
 ///////////////////ENDING////VIEW///////////////////
 function drawEndingRoom(names, cols, scores) {
     showView("ENDING");
-    // var table = document.getElementById("scoreBoard");
-    // var tblBody = document.createElement("tbody");
 
-    // for(let i = 0; i<length(names);i++) {
-    //     let row = document.createElement("tr");
-    //     let cell1 = document.createElement("td");
-    //     let cell2 = document.createElement("td");
+    var table = document.getElementById("scoreBoard");
+    var tblBody = document.createElement("tbody");
 
-    //     let player = document.createTextNode(""+names[i]);
-    //     let score = document.createTextNode(""+scores[i]);
-    //     player.style.color=""+cols[i]
+    for(let i = 0; i<names.length;i++){
+        let row = document.createElement("tr");
+        let cell1 = document.createElement("td");
+        let cell2 = document.createElement("td");
 
-    //     cell1.appendChild(player);
-    //     cell2.appendChild(score);
-    //     row.appendChild(cell1,cell2)
-    //     tblBody.appendChild(row);
-    //     table.appendChild(tblBody);
+        let player = document.createTextNode(""+names[i]);
+        let score = document.createTextNode(""+scores[i]);
+        cell1.style.color=cols[i]
 
-    // }
+        cell1.appendChild(player);
+        cell2.appendChild(score);
+        row.appendChild(cell1);
+        row.appendChild(cell2)
+        tblBody.appendChild(row);
+        table.appendChild(tblBody);
+    }
 }
 
 function emptyScoreBoard() {
